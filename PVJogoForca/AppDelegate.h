8d39f94ed5d8b469cd81447991b3de444a351a03
Rodolpho Pinto <rodolphopinto@MacBook-Pro-de-Rodolpho.local>
2013-11-26 15:47:46 -0200

    Initial Commit

---
+++ PVJogoForca/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PVJogoForca
+//
+//  Created by Rodolpho Pinto on 26/11/13.
+//  Copyright (c) 2013 Rodolpho Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PVJogoForca/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PVJogoForca
+//
+//  Created by Rodolpho Pinto on 26/11/13.
+//  Copyright (c) 2013 Rodolpho Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
